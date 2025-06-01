235dd8d69231ca134719b1b7d7098270b5cadb94
Md Masoom <rizmasoom786@gmail.com>
2025-06-02 00:33:32 +0545

    best seller component done

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -21,7 +21,7 @@ const LatestCollection = () => {
       {/* rendering products called latest collection upto 0 to 10 */}
 
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4
-      lg:grid-col-5 gap-4 gap-y-6'>
+      lg:grid-cols-5 gap-4 gap-y-6'>
         {
         latestProducts.map((item,index)=>(
           <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>

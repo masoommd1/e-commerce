c07ad8c181584d8761762c00bc88002a5cc06e7b
Md Masoom <rizmasoom786@gmail.com>
2025-06-11 15:02:42 +0545

    collection filter done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-10 my-10 mt-40 text-sm'>
+        
+        <div>
+           <img src={assets.logo} alt="" className='mb-5 w-32'/>
+           <p className='w-full md:w-2/3 text-gray-600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eveniet optio deserunt totam vitae eius fugit,
+             veritatis doloribus id possimus! Quo officia ex harum ipsa repudiandae pariatur quibusdam accusamus asperiores.
+           </p>
+        </div>
+        <div>
+           <p className='text-xl font-medium mb-5'>COMPANY</p>
+           <ul className='text-gray-600 flex flex-col gap-1'>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+           </ul>
+        </div>
+        <div>
+           <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+           <ul className='text-gray-600 flex flex-col gap-1'>
+            <li>+1-123-654-7896</li>
+            <li>rizmasoom786@gamil.com</li>
+           </ul>
+        </div>
+      </div>  
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2025@mdforever.com - All Rigth Reserved.</p>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import { assets } from '../assets/assets';
 
 const collection = () => {
+
+  const {products} = useContext(ShopContext)
+  const [showFilter,setShowFilter] = useState(false);
   return (
-    <div>collection</div>
+    
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+
+      {/* filter options  */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center gap-2 cursor-pointer'>FILTERS</p>
+        <img src={assets.dropdown_icon} className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} alt="" />
+        {/* category filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' :'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Men'}/>Men
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Women'}/>Women
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Kids'}/>Kids
+              </p>
+          </div>
+        </div>
+        {/* sub categorgy filter */}
+        <div>
+           <div className={`border border-gray-300 pl-5 py-3 my-5 ${showFilter ? '' :'hidden'} sm:block`}>
+          <p className='mb-3 text-sm font-medium'>Type</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Top-wear'}/>Top-wear
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Bottom-wear'}/>Bottom-wear
+              </p>
+              <p className='flex gap-2'>
+                <input className='w-3' type="checkbox" value={'Winter wear'}/>Winter wear
+              </p>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

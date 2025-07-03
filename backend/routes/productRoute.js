ebaa32fff72cb1c4c048693957ecb8ba7ae0554a
Md Masoom <rizmasoom786@gmail.com>
2025-07-03 13:25:15 +0545

    app product completed

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {addProduct,listProducts,removeProduct,singleProduct} from '../controllers/productController.js';
+import upload from '../middleware/multer.js';
+
+const productRouter = express.Router();
+
+// router for adding product
+productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1}
+                                        ,{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
+
+// router for removing product
+productRouter.post('/remove',removeProduct);
+  
+// router for single  product 
+productRouter.post('/single',singleProduct);
+
+// router for getting list product
+productRouter.get('/list',listProducts);
+
+export default productRouter;

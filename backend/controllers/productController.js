a261a9bfaab317a494ccc59d8f0df3fdbab9d1e7
Md Masoom <rizmasoom786@gmail.com>
2025-07-03 16:04:40 +0545

    logic done for login and crud

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -50,17 +50,38 @@ const addProduct = async (req,res) => {
 
 // function for listing  product
 const listProducts = async (req,res) => {
-
+    try {
+        const products = await productModel.find({});
+        res.json({success:true,products});
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    
+}
 }
 
 // function for deleting product
 const removeProduct = async (req,res) => {
-
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({success:true,message:'Product removed'});
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message});
+    }
 }
 
 // function for single  product inf0
 const singleProduct = async (req,res) => {
+    try {
+        const {productId} = req.body;
+        const product = await productModel.findById(productId);
+        res.json({success:true,product})
 
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message});
+    }
 }
 
-export {addProduct,listProducts,removeProduct,singleProduct};
+export { addProduct,listProducts,removeProduct,singleProduct } 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import {addProduct,listProducts,removeProduct,singleProduct} from '../controllers/productController.js';
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
 // router for adding product
-productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1}
+productRouter.post('/add',adminAuth,upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1}
                                         ,{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
 
 // router for removing product
-productRouter.post('/remove',removeProduct);
+productRouter.post('/remove',adminAuth,removeProduct);
   
 // router for single  product 
 productRouter.post('/single',singleProduct);

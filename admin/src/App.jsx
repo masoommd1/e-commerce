c54b1d6867542d6f132b66330f9639edd3b1c837
Md Masoom <rizmasoom786@gmail.com>
2025-07-04 23:45:58 +0545

    admin panel going on

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import SideBar from './components/SideBar'
 
 const App = () => {
   return (
     <div className=' bg-gray-50 min-h-screen'>
     <>
      <NavBar/>
-     <hr /> 
+     {/* <hr />     */}
+     <hr className="border-t border-gray-300 w-full " />
+     <div className='flex w-full'>
+     <SideBar/>
+     </div>
     </>
     </div>
   )
